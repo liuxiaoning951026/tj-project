bf83f33826d8905b53a6556e3b816290c2265b6e
liuning <liuning@idss-cn.com>
2020-12-03 11:21:07 +0800

    config/index.js文件增加port注释

--- config/index.js
+++ config/index.js
@@ -14,7 +14,7 @@ module.exports = {
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
-    port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    port: 8080, // npm run serve 编译时读取的端口号
     autoOpenBrowser: false,
     errorOverlay: true,
     notifyOnErrors: true,
